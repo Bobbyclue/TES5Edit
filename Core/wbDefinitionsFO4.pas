d808f608c24ea016c29192cc696a15b30238a6b9
robertgk2017 <robertgk2017@gmail.com>
2026-04-03 23:17:09 -0400

    And another one

--- Core/wbDefinitionsFO4.pas
+++ Core/wbDefinitionsFO4.pas
@@ -1475,21 +1475,6 @@ begin
   end;
 end;
 
-function wbSubrecordSizeDecider(aBasePtr: Pointer; aEndPtr: Pointer; const aElement: IwbElement): Integer;
-var
-  SubRecord : IwbSubRecord;
-begin
-  Result := 0;
-  if not Assigned(aElement) then
-    Exit;
-
-  if not Supports(aElement, IwbSubRecord, SubRecord) then
-    Exit;
-
-  if SubRecord.DataSize > 0 then
-    Result := 1;
-end;
-
 function wbFLSTLNAMIsSorted(const aContainer: IwbContainer): Boolean;
 var
   rEDID      : IwbRecord;
